cd03fe4a1f83f55b340302ba1b4c4b8b1386816d
Oleh Zahorulko <oleg.zago@gmail.com>
2018-04-03 22:33:50 +0200

    Add function renderArticle

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,26 +2,44 @@ var modalSaveBtn = document.getElementById('modal-save-btn');
 modalSaveBtn.addEventListener('click', function () {
     var title = document.getElementById('new-article-title').value;
     var content = document.getElementById('new-article-content').value;
-    var article = createArticle(title, content);
+    var article = new Article(title, content);
     localStorage.setItem('article', JSON.stringify(article));
     renderArticle(article);
 });
 
-function createArticle(title, content) {
+function Article(title, content) {
+    this.id = getArticleId();
+    this.title = title;
+    this.content = content;
+    this.creationDate = new Date();
 
     function getArticleId() {
         //TODO решить потом
         return 123;
     }
-
-    return {
-        id: getArticleId(),
-        title: title,
-        content: content,
-        creationDate: new Date()
-    }
 }
 
 function renderArticle(article) {
-    //TODO создать дом элемент стати и вывести на экран
+    // TODO создать элемент
+    // 1 создать артикл
+    var articleElem = document.createElement('article');
+    articleElem.setAttribute('class', 'article');
+
+    // 2 создать header и поместить в article
+    var header = document.createElement('header');
+    articleElem.appendChild(header);
+
+
+    // 3 создать main и поместить в article
+    var main = document.createElement('main');
+    articleElem.appendChild(main);
+
+    // 4 создать footer и поместить в article
+    var footer = document.createElement('footer');
+    footer.setAttribute('class', 'clearfix');
+    articleElem.appendChild(footer);
+
+    // TODO поместить в нужное место дом дерева
+    var articleContainer = document.getElementById('article-container');
+    articleContainer.appendChild(articleElem);
 }
